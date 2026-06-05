817c0f699a857ba64ae614a5551e2839d252dd91
blossomflower <israelramospessoal@gmail.com>
2026-06-04 22:05:55 -0300

    update: updating to cloud

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Logo from './assets/images/logo-universal.png'
+import { LuUpload, LuX } from "react-icons/lu";
 
 function App() {
 
+    const [isHovered, setIsHovered] = useState(false);
+    const [files, setFiles] = useState([]);
+    const [imageCount, setImageCount] = useState(0);
 
+    //* Lógica de tratamento do Drag and drop
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        setIsHovered(true)
+    }
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        setIsHovered(false);
+    }
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setIsHovered(false)
+
+        //converte os arquivos em array JS
+        const droppedFiles = Array.from(e.dataTransfer.files);
+        setFiles(files.concat(droppedFiles));
+    }
+    const handleInput = (e) => {
+        e.preventDefault();
+        let newFiles = Array.from(e.target.files);
+        setFiles(files.concat(newFiles));
+    };
+    //* fazer o fetch aqui pro backend pegar os arquivos
+    // useEffect(() => {
+    //     console.log(Array.from(files));
+    //     fetch('https://localhost:8000', {
+    //         "method": "post",
+    //     })
+    // }, [files])
+
+    useEffect(() => {
+        let buttons = document.getElementsByClassName('image_preview_x');
+        console.log(typeof (Array.from(buttons)));
+        Array.from(buttons).forEach((button) => {
+            button.addEventListener('click', (e) => {
+                let itemToBeRemoved = e.target.dataset.target;
+                itemToBeRemoved.remove();
+            })
+        })
+    }, [files]);
 
     return (<>
         <section className='nav'>
@@ -22,13 +66,26 @@ function App() {
         </section>
         <section className='header'>
             <div className='header_wrapper container'>
-                <div className='header_dropper_box'>
+                <div className='header_dropper_box' onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop}>
                     <div className='dropper_img_wrap'>
-                        {/* //! trocar por svg de upload */}
-                            <img src={Logo} width={'30px'} className='dropper_img'></img></div>
+                        <LuUpload /></div>
                     <h3>Selecionar Imagem(ns)</h3>
-                    <p>Arraste & Solte ou <a href='#' className='dropper_link'>Escolha</a></p>
+                    <p>Arraste & Solte ou <span className='highlight'>Escolha</span></p>
+                    <input type='file' accept='image/webp,image/jpeg,image/jpg,image/png,image/avif' id='file-input' multiple onChange={handleInput} />
                 </div>
+                <ul className='header_images_container'>
+                    {files.map((file, i) => {
+                        return (
+                            <li className={`image_preview`} id={`image_preview-${i}`} key={`image_preview-${i}`}>
+                                <img src={URL.createObjectURL(file)} alt={file.name} height={100} />
+                                <button className={'image_preview_x'} data-target={`image_preview-${i}`}>
+                                    <LuX />
+                                </button>
+
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </section>
     </>
