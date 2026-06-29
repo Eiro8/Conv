82ae8ee4956e8d13344460664ecb63b7338571a7
blossomflower <israelramospessoal@gmail.com>
2026-06-28 21:57:32 -0300

    feature(convert/filecard.jsx): adiciona visualizacao do tamanho de arquinho no filecard

--- frontend/src/pages/Convert/components/FileCard/FileCard.jsx
+++ frontend/src/pages/Convert/components/FileCard/FileCard.jsx
@@ -2,33 +2,54 @@ import React from 'react'
 import { useState } from 'react'
 import styles from './filecard.module.css'
 import { Button } from '../../../../components/ui/Button/Button'
-import { LuX, LuHardDriveDownload, LuChevronDown } from "react-icons/lu";
+import { LuX, LuHardDriveDownload, LuChevronDown, LuArrowLeftFromLine, LuArrowRight } from "react-icons/lu";
+
+function convertSize(size) {
+    let kilobyte = 1024;
+    let megabyte = kilobyte * 1024;
+    let gigabyte = megabyte * 1024;
+    let fileSize = ""
+
+    if (size >= gigabyte) {
+        fileSize = `${(size / gigabyte).toFixed(1)} MB`
+    } else if (size >= megabyte) {
+        fileSize = `${(size / megabyte).toFixed(1)} KB`
+    } else if (size >= kilobyte) {
+        fileSize = `${(size / kilobyte).toFixed(1)} KB`
+    } else {
+        fileSize = `${size} bytes`
+    }
+    return fileSize
+}
 
 const FileCard = ({
     file,
     allowedFileTypes,
-
     handleCloseButton,
     handleSave,
     ...props
 }) => {
-    const { id, name, type, src, path, isConverted, convertPath, convertTo, size = 2000 } = file;
+    const { id, name, type, src, path, isConverted, convertPath, convertTo, size = "SIX SEVENNN ( erro )" } = file;
     //* Receber o size em bytes para melhorar
-    const [fileSize, setFileSize] = useState({
-        size: size,
-        format: "KB"
-    });
     const [open, setOpen] = useState(false);
     const [selector, setSelector] = useState(convertTo);
+    let convertedFileSize = 232;
 
-    
+    let oldSize = convertSize(size);
+    let newSize = convertSize(convertedFileSize)
+
 
     return (
         <li className={styles.file}>
             <img className={styles.image} src={src} draggable={false} />
             <div className={styles.description}>
                 <p className={`${styles.name} text_overflow`}>{name}</p>
-                <p className={`${styles.type} text_overflow`}>{type}, <span>{fileSize.size}{fileSize.format}</span></p>
+                <span className={styles.info}>
+                    <p className={`${styles.type} text_overflow`}>{type}, <span>{oldSize}</span></p>
+                    {isConverted ? (<p className={styles.newType}><LuArrowRight />{newSize}</p>) : (null)}
+                </span>
+
+
             </div>
 
 
