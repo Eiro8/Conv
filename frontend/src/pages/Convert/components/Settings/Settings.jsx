ce01bb03f4fbe23fb89f6bdc5e31c614f659104a
blossomflower <israelramospessoal@gmail.com>
2026-07-18 01:40:26 -0300

    style(settings.jsx): adicione estilização basica para settings.jsx

--- frontend/src/pages/Convert/components/Settings/Settings.jsx
+++ frontend/src/pages/Convert/components/Settings/Settings.jsx
@@ -20,6 +20,7 @@ const SettingsButton = ({
 }) => {
     const [open, setOpen] = useState(false);
     const ConfigForm = useRef();
+    const [convertPercentage, setConvertPercentage] = useState(convertQuality)
 
 
     function handleSubmit(e) {
@@ -33,15 +34,28 @@ const SettingsButton = ({
                 (
                     <div className={styles.config_background}>
                         <div className={styles.config_options}>
-                            <Button children={<LuX />} onClick={() => setOpen(false)} />
+                            <span className={styles.close_button}>
+                                <Button children={<LuX />} onClick={() => setOpen(false)} />
+                            </span>
+                            <h3 className={styles.config_title}>Configurações</h3>
                             <form className={styles.options_box} ref={ConfigForm}>
-                                <h2>Configurações</h2>
-                                <span>
-                                    <button type='button' onClick={handleDirectorySelector}><LuFile /></button>
-                                    <input type='text' name={"save_directory"} placeholder="ex: C:\User\CR7\GOAT" value={saveDirectory} onChange={handleDirectorySelectorInput} ></input>
-                                </span>
-                                <input className="option_range" name="quality_range" type='range' min={0} max={100} step={1} defaultValue={convertQuality}></input>
-                                <button type="submit" name="submit_button" onClick={handleSubmit}>Salvar</button>
+                                <div className={styles.options_wrapper}>
+                                    <div className={styles.option}>
+                                        <label className={styles.config_label} htmlFor={"directory-input"}>Salvar em:</label>
+                                        <span className={styles.inputBox}>
+                                            <button className={styles.config_button} id={"directory-input"} type={"Button"} onClick={handleDirectorySelector} ><LuFile /></button>
+                                            <input className={styles.config_input} id={"directory-input"} type='text' name={"save_directory"} placeholder="ex: C:\User\CR7\GOAT" value={saveDirectory} onChange={handleDirectorySelectorInput} ></input>
+                                        </span>
+                                    </div>
+                                    <div className={styles.option}>
+                                        <label className={styles.config_label} htmlFor={"options-range"}>Qualidade de conversão:</label>
+                                        <span className={styles.rangeBox}>
+                                            <input className={styles.config_range} id={"options-range"} name="quality_range" type='range' min={0} max={100} step={1} defaultValue={convertPercentage} onChange={(e) => { setConvertPercentage(e.target.value) }}></input>
+                                            <span className={styles.config_percentage}>{convertPercentage}%</span>
+                                        </span>
+                                    </div>
+                                </div>
+                                <button className={styles.button} id={"submit-button"} type="submit" name="submit_button" onClick={handleSubmit}>Salvar</button>
                             </form>
                         </div>
                     </div>
